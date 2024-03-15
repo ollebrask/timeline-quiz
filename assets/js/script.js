f2bb6db7f41da91af634bc313234e7cc4503e136
ollebrask <ollstah@gmail.com>
2024-03-15 15:03:04 +0100

    Add questions and function for random question.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,11 @@ const beforeButton = document.getElementById('before-btn');
 const afterButton = document.getElementById('after-btn');
 const nextButton = document.getElementById('next-btn');
 const questionBoxElement = document.getElementById('question-box');
+const questionElement = document.getElementById('question');
+
+//variables for shuffling questions and keeping track of currentquestion.
+let shuffledQuestion;
+let currentQuestion;
 
 //eventlisteners for clicking buttons
 startButton.addEventListener('click', startQuiz);
@@ -15,8 +20,18 @@ nextButton.addEventListener('click', selectNext);
 function startQuiz() {
     console.log('started');
     startButton.classList.add('hide');
+    shuffledQuestion = questionsData.sort(() => Math.random() - .5);
+    currentQuestion = 0;
     questionBoxElement.classList.remove('hide');
     nextQuestion();
+}
+
+function nextQuestion() {
+    showQuestion(shuffledQuestion[currentQuestion]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
 }
 
 function selectBefore() {
@@ -30,3 +45,14 @@ function selectAfter() {
 function selectNext() {
     console.log('Next');
 }
+
+//questions for the quiz
+const questionsData = [{
+        "question": "The tower blablabla has a big restaurang at the the top, was it invented before or after 1950?",
+        "correctYear": 1991
+    },
+    {
+        "question": "The big bang was blablabla, was it before or after 1950?",
+        "correctYear": 1946
+    }
+]
